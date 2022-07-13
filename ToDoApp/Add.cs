25b67c027a485411f437cc4fd3411c522e75b716
Martin Hampel <martinhampel98@gmail.com>
2022-07-13 12:10:14 +0200

    Add project files.

---
+++ ToDoApp/Add.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ToDoApp
+{
+    internal class Add
+    {
+        private TextBox? nameBox;
+        private TextBox? descriptionBox;
+        private Window? Window;
+        private Window? mainWindow = (MainWindow)Application.Current.MainWindow;
+        private string path = @Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\ToDoApp\\";
+
+        public Add()
+        {
+            nameBox = ((MainWindow)Application.Current.MainWindow).addWindow?.nameTextBox;
+            descriptionBox = ((MainWindow)Application.Current.MainWindow).addWindow?.desctiptionTextBox;
+            Window = ((MainWindow)Application.Current.MainWindow).addWindow;
+        }
+
+        public void Cancel()
+        {
+            nameBox = null;
+            descriptionBox = null;
+            Window?.Close();
+        }
+
+        public void AddNewTask()
+        {
+            Save();
+        }
+        private void Save()
+        {
+            var fs = new FileStream(path + "data", FileMode.Append);
+            try
+            {
+                Save(fs);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Exception in Add.Save() method: " + ex);
+            }
+            finally
+            {
+                fs.Flush();
+                fs.Close();
+            }
+        }
+
+        private void Save(Stream fs)
+        {
+            var data = nameBox?.Text + "\n" + descriptionBox?.Text;
+            var bytes = Encoding.UTF8.GetBytes(data);
+            fs.Write(bytes, 0, bytes.Length);
+        }
+
+        private void DoAddNewTask()
+        {
+            var name = nameBox?.Text;
+            var description = descriptionBox?.Text;
+
+        }
+    }
+}
---
+++ ToDoApp/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ToDoApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public AddWindow? addWindow;
+        public MainWindow()
+        {
+            InitializeComponent();
+            CreateDirectory();
+        }
+
+        private void CreateDirectory()
+        {
+            try
+            {
+                Directory.CreateDirectory(@Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\ToDoApp\\");
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Exception in CreateDirectory() method: " + ex);
+            }
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            addWindow = new AddWindow();
+            addWindow.Show();
+        }
+    }
+}
